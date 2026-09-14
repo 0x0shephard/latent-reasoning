052cafdd58ba2305e295ed568c859478f4ba1b34
0x0shephard <lightenedproficiency@gmail.com>
2026-09-14 11:38:38 +0500

    Pin four-split KV notebook

--- scripts/build_kaggle_codi_preanswer_kv_subspaces_notebook.py
+++ scripts/build_kaggle_codi_preanswer_kv_subspaces_notebook.py
@@ -11,7 +11,7 @@ except ModuleNotFoundError:
     from scripts.notebook_compat import nbf
 
 OUTPUT = ROOT / "notebooks" / "kaggle_codi_preanswer_kv_subspaces.ipynb"
-RUN_COMMIT = "d1909156a690d26a02dddb6faffaf663371a8ebc"
+RUN_COMMIT = "b4fa08c9b97b123ccc459bba6e4ae294fd9f4d52"
 nb = nbf.v4.new_notebook()
 cells = []
 
